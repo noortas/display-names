bd4b981eecec72b9234d39066ce7bf041b8948e8
Rizwan Ahamed <rizwan@rizwan.in>
2016-11-30 11:50:00 +1100

    removed unnecessary files

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var displayNames = require('./index');
+
+describe('displayNames', function() {
+	it('should work!', function() {
+		expect(true).to.be.true;
+		
+	});
+});
+
+ 
